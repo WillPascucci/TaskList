cd6f8f06174975b4bddd80315b330592929c2e35
sahilpanjwani <sahilpanjwani12@gmail.com>
2015-04-13 10:42:35 -0500

    Slight edit in setPriority method in TaskList.java

--- app/src/main/java/com/github/willpascucci/tasklist/model/TaskList.java
+++ app/src/main/java/com/github/willpascucci/tasklist/model/TaskList.java
@@ -51,13 +51,13 @@ public class TaskList {
 
     public static void setPriority(Task t1){
         final Calendar c = Calendar.getInstance();
-        t1.timeRequired = (float) t1.getDeadline().getTime()-c.getTime().getTime();
+        float timeRemaining = (float) t1.getDeadline().getTime()-c.getTime().getTime();
         t1.elapsedTime = (long) c.getTime().getTime()-t1.getTime().getTime();
-        float timeRemaining = t1.timeRequired - t1.elapsedTime;
+        //float timeRemaining = t1.timeRequired - t1.elapsedTime;
         if (timeRemaining < 0) timeRemaining = 0.0f;
         t1.priority = t1.importance * 10;                             //get a score out of 100
-        if ((timeRemaining/t1.timeRequired) < 0.5){                         //less than half time left
-            if ((timeRemaining/t1.timeRequired) < 0.75) t1.priority += 40;   //less than quarter time left, increase priority
+        if ((timeRemaining/t1.elapsedTime) < 1.0){                         //less than half time left
+            if ((timeRemaining/t1.elapsedTime) < 0.5) t1.priority += 40;   //less than quarter time left, increase priority
             else t1.priority += 20;                                          //increase priority by a bit less for half time
         }
         t1.save();
